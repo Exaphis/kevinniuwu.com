263cfc78faac62fcdac05cefcc4c175b99dff445
Kevin Wu <kevin@kevinniuwu.com>
2022-07-01 20:52:08 -0400

    Add nested CSS support and custom Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#D9A629",
+        background: "#08122B",
+        text: "#CCCCCC",
+      },
+      spacing: {
+        112: "28rem",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+    },
   },
   plugins: [],
 };
