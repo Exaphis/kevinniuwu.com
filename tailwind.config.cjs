7ee0c2a8f17bd475097a914f5848d28b6de222db
Kevin Wu <kevin@kevinniuwu.com>
2022-07-30 15:10:50 -0400

    Fix countdown, upgrade Astro to 1.0.0rc3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,46 @@ module.exports = {
       spacing: {
         112: "28rem",
       },
+      animation: {
+        rainbow: "rainbow 25s infinite linear",
+      },
+      keyframes: {
+        rainbow: {
+          "0%": {
+            "background-color": "#ff000080",
+          },
+          "10%": {
+            "background-color": "#ff800080",
+          },
+          "20%": {
+            "background-color": "#ffff0080",
+          },
+          "30%": {
+            "background-color": "#80ff0080",
+          },
+          "40%": {
+            "background-color": "#00ff0080",
+          },
+          "50%": {
+            "background-color": "#00ff8080",
+          },
+          "60%": {
+            "background-color": "#00ffff80",
+          },
+          "70%": {
+            "background-color": "#0080ff80",
+          },
+          "80%": {
+            "background-color": "#0000ff80",
+          },
+          "90%": {
+            "background-color": "#8000ff80",
+          },
+          "100%": {
+            "background-color": "#ff000080",
+          },
+        },
+      },
     },
     fontFamily: {
       sans: ["Inter", ...defaultTheme.fontFamily.sans],
