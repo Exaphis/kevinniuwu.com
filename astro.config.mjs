b2cefdf139681f9f19d51f1947cfe9b4f3e8fbc5
Kevin Wu <kevin@kevinniuwu.com>
2022-10-20 19:54:57 -0400

    Add sharp, fix project image sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [
+    tailwind(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
