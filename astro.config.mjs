05dbb2d9e7ae22b44ba84c1e15f0b7cbfe435707
Kevin Wu <kevin@kevinniuwu.com>
2022-10-20 19:33:26 -0400

    Update astro, @astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image()],
 });
