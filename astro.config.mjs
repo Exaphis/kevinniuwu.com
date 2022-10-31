3e31c6aea4787e81da2b72a99481e8d2f82ee945
Kevin Wu <kevin@kevinniuwu.com>
2022-10-31 16:47:37 -0400

    Add BuckeyeCTF, Discord posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +13,9 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
-    mdx(),
+    mdx({
+      remarkPlugins: [remarkToc, remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
   ],
 });
