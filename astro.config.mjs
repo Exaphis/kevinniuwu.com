465736ed8a08e51b2b7297a11bb48ba16379c942
Kevin Wu <kevin@kevinniuwu.com>
2023-04-24 00:31:24 -0400

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kevinniuwu.com",
   integrations: [
     tailwind(),
     image({
@@ -16,6 +18,11 @@ export default defineConfig({
     mdx({
       remarkPlugins: [remarkToc, remarkMath],
       rehypePlugins: [rehypeKatex],
+    }),
+    sitemap({
+      filter: (page) =>
+        page !== "https://kevinniuwu.com/countdown/" &&
+        page !== "https://kevinniuwu.com/resume/",
     }),
   ],
 });
